f91d2ed2e5a469914397b2b991db482efce9c8c6
1llionaires <paulchodev@gmail.com>
2022-03-24 20:45:21 -0700

    2022-03-24 coding challenge and answer

---
+++ CC-2022-03-24/app.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+//P: integers
+//R: return integers as strings
+//E: given 123, result "123"
+//   given 999, result "999"
+//   given 369, result "369"
+//PC: create a function that converts number to string
+function numberToString(num) {
+    return num.toString();
+}
