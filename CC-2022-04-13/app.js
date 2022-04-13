bae4f6f15ce6f4703ab5c3683281eb18ee6fae40
1llionaires <paulchodev@gmail.com>
2022-04-13 00:39:24 -0700

    2022-04-13 coding challenge and answer

---
+++ CC-2022-04-13/app.js
@@ -0,0 +1,20 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+//P:num
+//R:given number loop through and create str
+//E: given 1, result '1 sheep...'
+//   given 2, result '1 sheep...2 sheep...'
+//   given 3, result '1 sheep...2 sheep...3 sheep...'
+//PC: create a function that takes in an integer and concatenates it into a str ('[i] sheep...')
+var countSheep = function(num){
+    let str = ""
+    for(let i=1; i<num;i++) {str += `${i} sheep...`}
+    return str
+}
+
+//OR
+
+countSheep=n=>[...Array(n).keys()].map(x=>`${i} sheep...`).join
