b9f6f844e5e4399d5be3f195a760059084870d64
1llionaires <paulchodev@gmail.com>
2022-04-23 21:51:57 -0700

    2022-04-23 coding challenge and answers

---
+++ CC-2022-04-23/app.js
@@ -0,0 +1,22 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//P: all
+//R: five
+//E: given number, return 5
+//   given string, return 5
+//   given array, return 5
+//PC: create a function that will return 5 no matter the parameter
+function unusualFive(){
+    let five = 'fives'
+    return five.length
+}
+
+//OR
+
+function unusualFive(){
+    return 'fives'.length
+}
