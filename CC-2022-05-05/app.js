ed2b74d9acbab7c7aa2e217b88098e0e809d14d5
1llionaires <paulchodev@gmail.com>
2022-05-05 16:40:30 -0700

    2022-05-05 coding challenge and answer

---
+++ CC-2022-05-05/app.js
@@ -0,0 +1,12 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+//P: string
+//R: add string to array
+//E: given [], results "codewars"
+//PC: given an empty array on the website add a string to the array
+websites.push('codewars')
