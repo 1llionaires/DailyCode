6c55985490ba5cecc4df528fef056537b80b5575
1llionaires <paulchodev@gmail.com>
2022-05-11 23:28:04 -0700

    2022-05-11 coding challenge and answer

---
+++ CC-2022-05-11(2)/app.js
@@ -0,0 +1,20 @@
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+
+//P: exams and projects
+//R: depending on exam grades and number of projects determine final grades
+//E: given 100,12, result 100
+//   given 85,5, result 90
+//   given 55,3, result 75
+//PC: create a function that checks conditionals to determine final grade
+function finalGrade (exam, projects) {
+    return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+}
