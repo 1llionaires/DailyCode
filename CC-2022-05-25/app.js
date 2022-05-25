daac5ab3a69bcb68035f5512adf517911d027795
1llionaires <paulchodev@gmail.com>
2022-05-25 15:26:10 -0700

    2022-05-25 coding challenge and answer

---
+++ CC-2022-05-25/app.js
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+//P: integer
+//R: return double integer
+//E: given 1, result 2
+//   given 2, result 4
+//   given 4, result 8
+//PC: create a function that returns double the integer
+function doubleInteger(i) {
+    return 2 * i;
+}
