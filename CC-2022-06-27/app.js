478b4a2d9f4c0c0907e6190c5e5dc4c1dfac1dfb
1llionaires <paulchodev@gmail.com>
2022-06-27 19:11:59 -0700

    2022-06-27 coding challenge and answer

---
+++ CC-2022-06-27/app.js
@@ -0,0 +1,12 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+//P: array of integers
+//R: return array in reverse order
+//E: given [1,2,3,4] results, [4,3,2,1]
+//   given [9,2,0,7] results, [7,0,2,9]
+//   given [4,3,2,1] results, [1,2,3,4]
+//PC: create a function to reverse the order of the array
+
+function reverseList(list) {
+    return list.reverse();
+}
