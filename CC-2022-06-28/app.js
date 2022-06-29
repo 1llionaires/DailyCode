bcd5c3e9996ba1953d6f4a35dca38f99c8f76400
1llionaires <paulchodev@gmail.com>
2022-06-28 20:19:50 -0700

    2022-06-28 coding challenge and answer

---
+++ CC-2022-06-28/app.js
@@ -0,0 +1,13 @@
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+//P: const
+//R: pass the tests that are given
+//E: given a == false,  true, results `a != false`
+//   given !a == false, true, results `!a != false`
+//   given a == !a,     true, results `a != !a`
+//PC: create a const to pass all three tests
+const a = []
