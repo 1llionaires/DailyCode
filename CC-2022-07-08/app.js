c45120ddc660fe80e21131e17d9b6a3f3ed77f82
1llionaires <paulchodev@gmail.com>
2022-07-08 23:46:17 -0700

    :wq 022-07-08 coding challenge and answer

---
+++ CC-2022-07-08/app.js
@@ -0,0 +1,19 @@
+// The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+// Great coding <3
+
+//P: x and y
+//R: return x to power of y without using Math.pow()
+//E: given (1,701270), results 1
+//   given (2,2), results 4
+//   given (3,2), results 9
+//PC: create a function that returns x^y without using Math.pow()
+function power(x,y){
+    return x**y
+}
