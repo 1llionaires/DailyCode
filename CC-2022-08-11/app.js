6c8bc1d22bda45ba58aad212c07cc85056af30f7
1llionaires <paulchodev@gmail.com>
2022-08-11 10:48:14 -0700

    2022-08-11 coding challenge and answer

---
+++ CC-2022-08-11/app.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//P: number
+//R: square of number
+//E: given 3, results 9
+//   given 2, results 4
+//   given 4 results 16
+//PC: create a function that squares the number given as the parameter
+function square(num){
+    return num * num;
+}
