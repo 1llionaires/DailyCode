0946a1c54d220050eab3fe7207c9b9ea94c36803
1llionaires <paulchodev@gmail.com>
2022-10-19 15:10:22 -0700

    2022-10-19 coding challenge and answeR

---
+++ CC-2022-10-19/app.js
@@ -0,0 +1,17 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5,]),3);
+// getAverage([1,1,1,1,1,1,1,2]),1)
+
+//array of numbers no letters or special char, no empty array
+//return the average of array elements
+//create a function that reduces the array and divides by length 
+//return the floor of the average to round down to nearest int
+function getAverage(arr){
+    return Math.floor(arr.reduce((a,b)=>a+b)/arr.length)
+}
