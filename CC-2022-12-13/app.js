8c676e4a889d8095ad598ea44bdadd16a698ae0c
1llionaires <paulchodev@gmail.com>
2022-12-13 15:25:20 -0800

    2022-12-13 coding challenge and answers

--- CC-2022-12-13/app.js
+++ CC-2022-12-13/app.js
@@ -12,14 +12,18 @@
 // Output: 2
 
 function majorityElements(arr){
-    let elem = {}
-        count = 0
-        majElem = arr[0]
+    arr.sort()
+    return arr[Math.floor(arr.length/2)]
 
-    for (const num of arr){
-        elem[num] = elem[num] + 1 || 1
-        elem[num] > arr.length/2
-    }
+
+    // let elem = {}
+    //     count = 0
+    //     majElem = arr[0]
+
+    // for (const num of arr){
+    //     elem[num] = elem[num] + 1 || 1
+    //     elem[num] > arr.length/2
+    // }
 
     // for (const n in elem){
     //     if (elem[n]>count){
