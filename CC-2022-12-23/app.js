d18ddb54ef5e9e2cb5a89a554060b238119ca7ad
1llionaires <paulchodev@gmail.com>
2022-12-23 12:23:50 -0800

    2022-12-23 coding challenge and answer

---
+++ CC-2022-12-23/app.js
@@ -0,0 +1,35 @@
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+
+// use the given switch case and fix the errors to run correctly
+// given id return correct planet name
+// given 1, results 'Mercury'
+// given 3, results 'Earth'
+//change name to return
+
+
+function getPlanetName(id){
+    switch(id){
+      case 1:
+        return 'Mercury'
+      case 2:
+        return 'Venus'
+      case 3:
+        return 'Earth'
+      case 4:
+        return 'Mars'
+      case 5:
+        return 'Jupiter'
+      case 6:
+        return 'Saturn'
+      case 7:
+        return 'Uranus'
+      case 8:
+        return 'Neptune'
+    }
+  
+  }
