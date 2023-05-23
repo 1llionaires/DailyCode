2f2f65deabcd4a1cf470c9fb6d6844cb542f5620
1llionaires <paulchodev@gmail.com>
2023-05-23 11:24:29 -0700

    2023-05-23 coding challenge and answer

--- CC-2023-05-23/app.js
+++ CC-2023-05-23/app.js
@@ -1,3 +1,4 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-const evenOrOdd = (n) => n%2===0?'Even':'Odd'
+const evenOrOdd = (n) => n%2===0? 'Even':'Odd'
+
