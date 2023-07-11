25f31bf8c87284b950c9d6abe7a2a5c51f9427c5
1llionaires <paulchodev@gmail.com>
2023-07-11 13:37:03 -0700

    2023-07-11 coding challenge and answer

---
+++ CC-2023-07-11/app.js
@@ -0,0 +1,34 @@
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+ 
+
+// Example 1:
+
+// Input: callCount = 5
+// Output: [0,1,1,2,3]
+// Explanation:
+// const gen = fibGenerator();
+// gen.next().value; // 0
+// gen.next().value; // 1
+// gen.next().value; // 1
+// gen.next().value; // 2
+// gen.next().value; // 3
+// Example 2:
+
+// Input: callCount = 0
+// Output: []
+// Explanation: gen.next() is never called so nothing is outputted
+
+var fibGenerator = function*() {
+    let n1=0, n2 = 1
+    while(true){
+        yield n1
+        let temp = n2
+        n2 = n1 + n2
+        n1 = temp  
+    }
+};
